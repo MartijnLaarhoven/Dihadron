b1e5ec6a4f06aa870bb4b7ab98bfbbfbbb7a4a35
Martijn Laarhoven <martijnlaarhoven2001@gmail.com>
2026-03-03 13:32:28 +0100

    Add method comparison to pipeline scripts

--- eta-diHadronNeNe/eta-diHadronNeNe/Dihadron/clearAll.sh
+++ eta-diHadronNeNe/eta-diHadronNeNe/Dihadron/clearAll.sh
@@ -12,6 +12,7 @@ mkdir -p ProcessOutput/PeripheralSubtraction
 echo "Creating TemplateFit structure..."
 mkdir -p TemplateFit/PDFs
 mkdir -p TemplateFit/EtaDiff/PDFs
+mkdir -p TemplateFit/EtaDiff/MethodComparison
 mkdir -p TemplateFit/PeripheralSubtracted/EtaDiff/PDFs
 mkdir -p TemplateFit/Uncorrected_FourierFit/EtaDiff
 mkdir -p TemplateFit/Comparisons/PDFs
